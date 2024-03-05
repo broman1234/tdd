3e24888c2e8c250ef7d71621a2e1eeeafac3d517
MOMAN11 <thoughtworks.mo@extaccount.com>
2024-03-05 16:40:37 +0800

    use provider to get instances

--- src/main/java/com/example/tdd/Context.java
+++ src/main/java/com/example/tdd/Context.java
@@ -1,28 +1,32 @@
 package com.example.tdd;
 
+import jakarta.inject.Provider;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Context {
-    Map<Class<?>, Object> instances =new HashMap<>();
-    Map<Class<?>, Class<?>> implementations = new HashMap<>();
+    Map<Class<?>, Provider<?>> providers = new HashMap<>();
+
     public <Component> void bind(Class<Component> type, Component instance) {
-        instances.put(type,instance);
+        providers.put(type, (Provider<Component>) () -> instance);
     }
 
     public <Component> Component get(Class<?> componentClass) {
-        if (instances.containsKey(componentClass))
-            return (Component) instances.get(componentClass);
-        try {
-            return (Component) implementations.get(componentClass).getDeclaredConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
+        return (Component) providers.get(componentClass).get();
+
     }
 
 
     public <Component, Implementation extends Component> void bind(Class<Component> componentClass, Class<Implementation> componentWithDefaultConstructorClass) {
-        implementations.put(componentClass,componentWithDefaultConstructorClass);
+        providers.put(componentClass, (Provider<Implementation>) () -> {
+            try {
+                return componentWithDefaultConstructorClass.getDeclaredConstructor().newInstance();
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                     NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
